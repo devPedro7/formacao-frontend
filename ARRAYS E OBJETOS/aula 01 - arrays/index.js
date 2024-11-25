7aa6a77d99cb8604dac986f9fd58cf348e02836a
Pedro Lucas de Sousa Ribeiro <devpedro37@gmail.com>
2024-11-25 12:11:23 -0300

    Adicionando a secao de Arrays e Objetos

---
+++ ARRAYS E OBJETOS/aula 01 - arrays/index.js
@@ -0,0 +1,14 @@
+//ARRAYS
+
+//estrutura do array
+const lista = [1,2,3,4,5]
+
+console.log(lista);
+console.log(typeof(lista)); // objeto
+
+
+//PODEMOS MESCLAR NO JAVASCRIPT DIFERENTES TIPOS DE DADOS EM UMA LISTA
+const array = [3,7,"Pedro", 3.17]
+
+console.log(array);
+console.log(typeof(array));
---
+++ ARRAYS E OBJETOS/aula 02 - propiedades/index.js
@@ -0,0 +1,5 @@
+//PODEMOS ACESSAR AS PROPIEDADES SÃO COMO INFORMAÇÕES DOS OBJETOS. CONSEGUIMOS ACESSAR ATRAVÉS DA NOTAÇÃO " . " OU DE "[nome da propiedade]"
+
+const numeros = [1,2,3]
+console.log(numeros.length); //através da notação "." conseguimos acessar a propiedade length que retorna o tamanho do array
+console.log(numeros["length"])//aqui tem a mesma funcção da notação ".";
