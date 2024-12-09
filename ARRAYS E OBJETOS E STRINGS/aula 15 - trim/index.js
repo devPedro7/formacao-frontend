5addccf405d9cb073eca4f404b46032ac37547e8
Pedro Lucas de Sousa Ribeiro <devpedro37@gmail.com>
2024-12-09 12:29:38 -0300

    Adicionando duas aulas de metodos de strings

---
+++ ARRAYS E OBJETOS E STRINGS/aula 15 - trim/index.js
@@ -0,0 +1,6 @@
+const frase = "o rato roeu a roupa do rei de roma"
+
+const arrayDaFrase = frase.split(" ")
+
+console.log(arrayDaFrase);
+console.log(arrayDaFrase[3]);
---
+++ ARRAYS E OBJETOS E STRINGS/aula 16 - join/index.js
@@ -0,0 +1,13 @@
+const frase = "O rato roeu a roupa do rei de roma"
+
+const arrayFrase = frase.split(" ")
+console.log(arrayFrase)
+
+const fraseString = arrayFrase.join(" ")
+console.log(fraseString);
+
+
+const itensCompra = ["Mouse", "Teclado", "Monitor"]
+console.log(itensCompra);
+
+const fraseCompra = console.log(itensCompra.join(", "))
